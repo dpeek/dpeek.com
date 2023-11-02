1e8f74286928aab3f7cf5f3592daa1f8a262401b
David Peek <mail@dpeek.com>
2023-11-02 22:57:19 +1100

    Catch errors in actions

--- app/actions.ts
+++ app/actions.ts
@@ -6,11 +6,11 @@ export async function trackView(pathname: string) {
   await fetch(endpoint + pathname, {
     method: "POST",
     cache: "no-store",
-  });
+  }).catch(() => {});
 }
 
 export async function getViews() {
   const res = await fetch(endpoint, { next: { revalidate: 5 } });
-  const json = await res.json();
+  const json = await res.json().catch(() => []);
   return json as { pathname: string; count: number }[];
 }
